08981d7b14546c91f10308c549d7829a0799a0b6
Jānis Ļubļinskis <j.lublinskis@gmail.com>
2020-03-16 12:22:21 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('entity_factory_bundle');
+        $treeBuilder = new TreeBuilder('entity_factory_bundle');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
